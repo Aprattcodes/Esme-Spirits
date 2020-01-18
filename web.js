4da4286da0877ebe699f3db69a5d01b062267f20
Alyssa Pratt <alyssakpratt@gmail.com>
2020-01-18 13:50:24 -0600

    adds bartender photo

--- web.js
+++ web.js
@@ -1,4 +1,22 @@
+
 function scrollFunction() {
     var elmnt = document.getElementById("glass");
     elmnt.scrollIntoView();
-  }
+  };
+
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  };
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  };
+
+  function ginClick() {
+    var gin = document.getElementById('ginLabel');
+    gin.addEventListener("click", function on(){
+    document.getElementById("overlay").style.display = "block";
+  })
+};
+
+  
