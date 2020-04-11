d6c035577b3a5c37e71d86473e874a1bd24086a0
Alyssa Pratt <alyssakpratt@gmail.com>
2020-04-11 15:22:35 -0500

    Adds Navigation bar as well as edits

--- web.js
+++ web.js
@@ -19,4 +19,10 @@ function scrollFunction() {
   })
 };
 
-  
+var colors = ['#fb5714', '#00ff00', '#0000ff'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.getElementById('rectangle').style.color = random_color;
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
